dfe932891c8d1bda78c7f6fa8267807774839142
Apoorv Anand <5118689+apoorv74@users.noreply.github.com>
2024-04-28 08:00:36 +0530

    update script to fetch case action details

--- scripts/get-case-proceedings.R
+++ scripts/get-case-proceedings.R
@@ -31,16 +31,19 @@ case_details_url <- "https://hrcnet.nic.in/HRCNet/public/CaseStatus.aspx"
 case_search_url <- "https://hrcnet.nic.in/HRCNet/public/SearchComplaint.aspx"
 remDr$navigate(case_search_url)
 
-i <- 30
 for(i in 1:length(case_numbers)){
   if(i > 1){
     # Switch Windows
     web_page_id <- remDr$getWindowHandles()[[1]]
     remDr$switchToWindow(windowId = web_page_id)
+    remDr$navigate(case_search_url)
+    
   }
-  
   # Enter Case Number
   case_num_i <- case_numbers[i]
+  
+  print(glue::glue("Case Number {i} - {case_num_i}"))
+  
   select_case_number <- remDr$findElement(using = "id","ContentPlaceHolder1_textFileNo")
   select_case_number$clearElement()
   select_case_number$sendKeysToElement(list(case_num_i))
@@ -53,10 +56,13 @@ for(i in 1:length(case_numbers)){
   search_element <- remDr$findElement(using = "id","ContentPlaceHolder1_btnSearch")
   search_element$clickElement()
   
+  Sys.sleep(5)
+  
   # Click on the case number
   case_details_page_xpath <- '//*[@id="ContentPlaceHolder1_GridViewSearResult_LinkButton1_0"]'
   case_details_page <- remDr$findElement(using = "xpath",value = case_details_page_xpath)
   case_details_page$clickElement()
+  Sys.sleep(5)
   
   # Switch Windows
   web_page_id <- remDr$getWindowHandles()[[2]]
@@ -68,12 +74,9 @@ for(i in 1:length(case_numbers)){
   list_of_actions <-
     case_details_html %>% read_html() %>% html_nodes(css = '#ContentPlaceHolder1_gridActionList') %>% html_table %>% pluck(1) 
   total_rows <- nrow(list_of_actions)
-
-  # Check for more pages in the Action List
-  action_numbers <- list_of_actions$`Action No.`
   
   # If number 2 is not present in the list of action numbers, then search for more pages
-  if(total_rows > 1 && !(2 %in% action_numbers)) {
+  if(total_rows > 10) {
     total_pages <-
       list_of_actions[nrow(list_of_actions), ] %>% unlist(use.names = FALSE) %>% na.omit() %>% as.vector() %>% length()
     for (j in 1:total_pages) {
@@ -82,7 +85,6 @@ for(i in 1:length(case_numbers)){
       list_of_actions <- actions_html %>% read_html() %>% html_nodes(css = '#ContentPlaceHolder1_gridActionList') %>% html_table %>% pluck(1) 
       }
       total_rows <- nrow(list_of_actions) - 2
-      sleep_time <- total_rows
       list_of_actions_j <- list_of_actions[1:total_rows, 1:4]
       all_actions <-
         dplyr::bind_rows(all_actions, list_of_actions_j)
@@ -93,11 +95,11 @@ for(i in 1:length(case_numbers)){
       expand_action_xpath_element$clickElement()
 
       # Sleep
-      Sys.sleep(sleep_time)
+      Sys.sleep(20)
       
       # Fetch all action details
       action_details_html <- remDr$getPageSource() %>% pluck(1)
-      action_details_page <- get_action_details(total_rows)
+      action_details_page <- get_action_details(action_details_html, total_rows)
       action_details <- dplyr::bind_rows(action_details, action_details_page) 
       
       if (j < total_pages) {
@@ -108,7 +110,7 @@ for(i in 1:length(case_numbers)){
         next_action_list_page <-
           remDr$findElement(using = "xpath", value = next_action_list_page_xpath)
         next_action_list_page$clickElement()
-        Sys.sleep(4)    
+        Sys.sleep(20)    
       }
     }
   } else {
@@ -122,11 +124,18 @@ for(i in 1:length(case_numbers)){
     expand_action_xpath_element$clickElement()
     
     # Sleep
-    Sys.sleep(sleep_time)
+    Sys.sleep(20)
     
     # Fetch all action details
     action_details_html <- remDr$getPageSource() %>% pluck(1)
-    action_details_page <- get_action_details(total_rows)
+    action_details_page <- get_action_details(action_details_html,total_rows)
     action_details <- dplyr::bind_rows(action_details, action_details_page) 
   }
-  }
+  
+  file_title_action_list <- glue::glue("data/nhrc_data_2020/case_actions/action_list/case_{i}_action_list.csv")
+  readr::write_csv(all_actions, file_title_action_list)
+  
+  file_title_action_details <- glue::glue("data/nhrc_data_2020/case_actions/action_details/case_{i}_action_details.csv")
+  readr::write_csv(action_details, file_title_action_details)
+  
+}
